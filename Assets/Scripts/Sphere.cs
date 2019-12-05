f9b3116281e034e1900a33458d37f94d2275e388
Ben Kelly <benkellysoftware@gmail.com>
2019-12-05 15:09:54 +1100

    Added Timer and Score interfaces

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class Sphere : MonoBehaviour
 {
     public Vector3 resetPos;
-
+    private Timer _timer;
+    private Score _score;
     private Rigidbody _rb;
+    
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _timer = GameObject.Find("Timer").GetComponent<Timer>();
+        _score = GameObject.Find("Score").GetComponent<Score>();
     }
 
     // Update is called once per frame
@@ -26,6 +30,11 @@ public class Sphere : MonoBehaviour
         {
             _rb.velocity = Vector3.zero;
             transform.position = resetPos;
+            _timer.AddTime(-2);
+        } else if (other.CompareTag("Goal"))
+        {
+            _timer.AddTime(4);
+            _score.AddScore(1);
         }
     }
 }
---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxTime = 30;
+    public float currentTime = 30;
+    void Start()
+    {
+        currentTime = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+        transform.localScale = new Vector3(0.95f * currentTime/ maxTime, 0.8f, 0.1f);
+    }
+
+    public void AddTime(float time)
+    {
+        currentTime += time;
+        if (currentTime > maxTime) currentTime = maxTime;
+    }
+}
