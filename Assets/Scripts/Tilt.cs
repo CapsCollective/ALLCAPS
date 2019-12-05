3c0f28c051f2da87f3fa9b691de7bbbc1b5af4d4
Ben Kelly <benkellysoftware@gmail.com>
2019-12-05 13:05:16 +1100

    Linear Reset Scaling

--- Assets/Scripts/Tilt.cs
+++ Assets/Scripts/Tilt.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Tilt : MonoBehaviour
 {
 	private Rigidbody _rb;
-	public float speedH, speedV;
+	public float speedH, speedV, speedReset;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -18,6 +18,7 @@ public class Tilt : MonoBehaviour
 		float h = Input.GetAxis("Horizontal") * Time.deltaTime * speedH;
 		float v = Input.GetAxis("Vertical") * Time.deltaTime * speedV;
 		_rb.AddTorque(new Vector3(v, 0, -h));
-
+		float step = speedReset * Time.deltaTime;
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), step);
 	}
 }
