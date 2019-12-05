139f24ed56f26d7d0ab34735aae8f54a4e8797ec
Jonathan Moallem <jdmoallem@gmail.com>
2019-12-06 09:45:16 +1100

    Added game overlay quad to main scene

---
+++ Assets/GameOverlay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameOverlay : MonoBehaviour
+{
+    public float colourOffset;
+    public GameObject timer;
+    private MeshRenderer _meshRenderer;
+    private Timer _timer;
+    
+    private void Start()
+    {
+        _timer = timer.GetComponent<Timer>();
+        Debug.Log(_timer.maxTime);
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var percent = 1 - (_timer.currentTime / _timer.maxTime) - colourOffset;
+        var color = _meshRenderer.material.color;
+        color.a = percent;
+        _meshRenderer.material.color = color;
+    }
+}
