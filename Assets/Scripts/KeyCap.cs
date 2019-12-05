27b4251d5ca06099b1d60b7f0a849d6b6f5def9f
Jonathan Moallem <jdmoallem@gmail.com>
2019-12-05 12:23:10 +1100

    Made keycaps move on key down

--- Assets/Scripts/KeyCap.cs
+++ Assets/Scripts/KeyCap.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class KeyCap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string key;
+    public float maxPos;
+    public float minPos;
+    
+    
+    private Vector3 _targetPos;
+    private const int DownSpeed = 5;
+    private const int UpSpeed = 10;
+
+    private void Start()
     {
-        
+        _targetPos = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var speed = 0;
+        if (Input.GetKey(key) && transform.localPosition.y > minPos)
+        {
+            _targetPos.y = minPos;
+            speed = DownSpeed;
+        }
+        else if (transform.localPosition.y < maxPos)
+        {
+            _targetPos.y = maxPos;
+            speed = UpSpeed;
+        }
+        transform.localPosition = Vector3.Lerp(transform.localPosition, _targetPos, speed * Time.deltaTime);
     }
 }
