6906d0d21461acafabb2c2c0bf0ae1adb1c3ebf7
Ben Kelly <benkellysoftware@gmail.com>
2019-12-11 15:31:28 +1100

    Adjusted menu to have an empty settings screen

---
+++ Assets/Scripts/MoveToView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveToView : MonoBehaviour
+{
+    public Vector3 menuPos, settingsPos;
+    
+    public void ViewSettings()
+    {
+        Debug.Log("Move");
+        StartCoroutine(Move(settingsPos));
+    }
+    
+    public void ViewMenu()
+    {
+        StartCoroutine(Move(menuPos));
+    }
+
+    private IEnumerator Move(Vector3 targetPos)
+    {
+        float t = 0;
+        Vector3 startPos = transform.position;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, targetPos, Mathf.SmoothStep(0.0f, 1.0f, t));
+            yield return null;
+        }
+    }
+}
