33de7a82218834a1c36f8fe113aec0e0ab7d3ffb
Ben Kelly <benkellysoftware@gmail.com>
2020-07-06 18:38:34 +1000

    Updated unity version, fixed settings look, added exit button, fixed flashing lights so its not an epilepsy hazard.

--- Assets/Scripts/GameOverlay.cs
+++ Assets/Scripts/GameOverlay.cs
@@ -6,20 +6,18 @@ public class GameOverlay : MonoBehaviour
     public GameObject timer;
     private MeshRenderer _meshRenderer;
     private Timer _timer;
-    
+    private float count = 0;
     private void Start()
     {
         _timer = timer.GetComponent<Timer>();
         _meshRenderer = GetComponent<MeshRenderer>();
     }
     
-    private void Update()
+    private void FixedUpdate()
     {
         var percentRemaining = 1 - (_timer.currentTime / _timer.maxTime);
-        var newAlpha = percentRemaining - 
-                           Mathf.PingPong(Time.time * 
-                                          Mathf.Clamp(percentRemaining/2,0.0f, 0.5f), 
-                               colourOffset);
+        count += Mathf.Clamp(percentRemaining, 0f, 0.6f) / 60f;
+        var newAlpha = percentRemaining - 0.2f - Mathf.PingPong(count, colourOffset);
         var color = _meshRenderer.material.color;
         color.a = newAlpha;
         _meshRenderer.material.color = color;
--- Assets/Scripts/SettingsHandler.cs
+++ Assets/Scripts/SettingsHandler.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-[System.Serializable]
 public class SettingsHandler : MonoBehaviour
 {
     private Dropdown _resolutionSelect;
@@ -15,67 +9,53 @@ public class SettingsHandler : MonoBehaviour
     private Slider _volumeSlider;
     
     private Resolution[] _resolutions;
-    
-    public float masterVolume = 1;
-    public bool isFullscreen = true;
-    public int resWidth = 1920;
-    public int resHeight = 1080;
-    
+
     void Start()
     {
         _resolutionSelect = GameObject.Find("ResolutionSelect").GetComponent<Dropdown>();
         _fullscreenToggle = GameObject.Find("FullscreenToggle").GetComponent<Toggle>();
         _volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
-        
-        LoadSettings();
-        
-        Screen.SetResolution(resWidth, resHeight, isFullscreen);
-        // Set resolution dropdown
+
         _resolutions = Screen.resolutions;
+        _resolutionSelect.ClearOptions();
+        List<string> options = new List<string>();
         for (int i = 0; i < _resolutions.Length; i++)
         {
-            string res = _resolutions[i].ToString();
-            _resolutionSelect.options.Add(new Dropdown.OptionData(res.Remove(res.Length-7)));
-            if (Screen.currentResolution.ToString().Equals(res))
-            {
-                _resolutionSelect.value = i;
-            }
+            string optionText = _resolutions[i].width + " x " + _resolutions[i].height + " " + _resolutions[i].refreshRate + "Hz";
+            options.Add(optionText);
         }
-        _fullscreenToggle.isOn = isFullscreen;
-        _volumeSlider.value = masterVolume;
-    }
-    
-    public void LoadSettings()
-    {
-        if (!File.Exists(Application.persistentDataPath + "/settings.json")) return;
-        string readString = File.ReadAllText(Application.persistentDataPath + "/settings.json");
-        JsonUtility.FromJsonOverwrite(readString, this);
-    }
-    
-    public void SaveSettings()
-    {
-        masterVolume = _volumeSlider.value;
-        var res = _resolutions[_resolutionSelect.value];
-        resHeight = res.height;
-        resWidth = res.width;
-        isFullscreen = _fullscreenToggle.isOn;
+        _resolutionSelect.AddOptions(options);
         
-        if (File.Exists(Application.persistentDataPath + "/settings.json"))
-        {
-            File.Delete(Application.persistentDataPath + "/settings.json");
-        }
-        File.WriteAllText(Application.persistentDataPath + "/settings.json", JsonUtility.ToJson(this));
-    }
-
-    public void SetResolution()
-    {
-        var res = _resolutions[_resolutionSelect.value];
-        Screen.SetResolution(res.width, res.height, _fullscreenToggle.isOn);
-    }
-
-    public void SetSound()
-    {
-        AudioListener.volume = _volumeSlider.value;
+        bool isFullscreen = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
+        SetFullScreen(isFullscreen);
+        _fullscreenToggle.isOn = isFullscreen;
+        
+        int res = PlayerPrefs.GetInt("Resolution", _resolutions.Length - 1); //Default to max res
+        SetResolution(res);
+        _resolutionSelect.value = res;
+        
+        float val = PlayerPrefs.GetFloat("Volume", _volumeSlider.maxValue);
+        SetVolume(val);
+        _volumeSlider.value = val;
     }
     
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        PlayerPrefs.SetInt("Resolution", resolutionIndex);
+    }
+
+    public void SetVolume(float volume)
+    {
+        _volumeSlider.value = volume;
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
 }
